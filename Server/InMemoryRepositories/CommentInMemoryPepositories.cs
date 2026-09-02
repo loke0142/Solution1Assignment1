ec197a65e5c8f0d9df58a5a1f1798505d984b5f5
loke0142 <lokehansen@gmail.com>
2026-09-02 11:43:37 +0200

    Done i guess

---
+++ Server/InMemoryRepositories/CommentInMemoryPepositories.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository
+{
+    private readonly List<Comment> comments;
+
+    public CommentInMemoryRepository()
+    {
+        comments = new List<Comment>();
+    }
+
+    
+}
---
+++ Server/InMemoryRepositories/UserInMemoryRepositories.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepositories
+{
+    private readonly List<User> users;
+
+    public UserInMemoryRepositories()
+    {
+        users = new List<User>();
+    }
+
+  
+}
